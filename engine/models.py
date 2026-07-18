e4757342ba45d56c2675f816c39a8cecbb8038f2
Jaewon Park <jaewonparkk73@gmail.com>
2026-07-17 20:04:03 -0400

    Add travel time calculation

--- engine/models.py
+++ engine/models.py
@@ -44,3 +44,32 @@ class Housekeeper:
 
     def clean_time(self, room: Room) -> float:
         return room.base_clean_time() * self.speed_multiplier
+    
+    def travel_time(self, room: Room) -> float:
+        floor_change_minutes = abs(self.start_floor - room.floor) * 3.0
+        hallway_minutes = abs(self.start_position - room.position) * 0.5
+
+        return floor_change_minutes + hallway_minutes
+    
+room = Room(
+    room_id="301",
+    floor=3,
+    position=20.0,
+    room_type=RoomType.SUITE,
+    occupancy_nights=3,
+    guests=2,
+    priority=2,
+    check_in_deadline_minute=180,
+)
+
+housekeeper = Housekeeper(
+    housekeeper_id="H1",
+    start_floor=1,
+    start_position=10.0,
+    speed_multiplier=0.9,
+    available_at_minute=0,
+    certified_for_suites=True,
+)
+
+print(housekeeper.clean_time(room))
+print(housekeeper.travel_time(room)) 
